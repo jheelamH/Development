b1e1a1b4aa7ab2a481bd29aea3c74d5c0ea8ce28
Jheelam <jheelamhossain147@gmail.com>
2025-02-01 15:53:42 +0530

    changes made

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,56 @@
 package org.example;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Employee {
+    String name;
+    int id;
+    int salary;
+
+    public Employee() {
+    }
+    public Employee(String name, int id, int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            String json = "{\"name\":\"John Doe\", \"id\":101, \"salary\":50000}";
+
+            Employee emp1 = objectMapper.readValue(json, Employee.class);
+
+            System.out.println(json);
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
